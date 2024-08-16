c890758958b494517589451f926be0f22d218aa0
xianyi <thefangyi@gmail.com>
2024-08-16 21:38:57 +0800

    fix: waiting for client connection & init

--- App.tsx
+++ App.tsx
@@ -5,6 +5,28 @@
  * @format
  */
 
+// 修复 waiting for client connection
+import {DevSettings, NativeModules} from 'react-native';
+
+const addDebugMenuItems = async () => {
+  const message = {
+    stop: '(*) Stop Debugging',
+    debug: '(*) Debug JS Remotely',
+  };
+
+  DevSettings.addMenuItem(message.debug, () => {
+    NativeModules.DevSettings.setIsDebuggingRemotely(true);
+  });
+  DevSettings.addMenuItem(message.stop, () => {
+    NativeModules.DevSettings.setIsDebuggingRemotely(false);
+  });
+};
+
+export const enableDebugging = async () => {
+  setTimeout(addDebugMenuItems, 100);
+};
+
+
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -29,32 +51,6 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -68,51 +64,17 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      
+      <View>
+        <Text>Hello World</Text>
+      </View>
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
